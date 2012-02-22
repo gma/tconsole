e035346f3f03487409b9e286c4720cb7a6d1145f
Alan Johnson <alan@commondream.net>
2012-02-21 20:53:46 -0500

    Started working on pipe server technique.

--- pipe_bomb.rb
+++ pipe_bomb.rb
@@ -6,12 +6,17 @@ class PipeServer
     @callee[0], @caller[1] = IO.pipe
     @caller[0], @callee[1] = IO.pipe
 
+    @is_caller = false
+    @is_callee = false
+
     @me = nil
   end
 
   # Identifies the current process as the callee process
   def callee!
     @me = @callee
+    @is_callee = true
+    @is_caller = false
 
     @caller.each do |io|
       io.close
@@ -21,12 +26,39 @@ class PipeServer
   # Identifies the current process as the caller process
   def caller!
     @me = @caller
+    @is_caller = true
+    @is_callee = false
 
     @callee.each do |io|
       io.close
     end
   end
 
+  # Sends a message over to the client and gets a response back.
+  # Raises an error if not used on the caller side of the pipe
+  # server. Returns the result received back from the pipe
+  # server.
+  #
+  # Message parameter can be anything that marshals cleanly.
+  def send(message)
+    raise "You can't send from a PipeServer callee." unless @is_caller
+
+    write(message)
+    read
+  end
+
+  # Blocks until a message is received, and then runs block on it with
+  # the message as a parameter.
+  #
+  # Should only be run on the callee side of the pipe
+  def process_messages(&block)
+    while message = read
+      response = block.call(message)
+      write(response)
+    end
+  end
+
+private
   # Writes a message to the appropriate pipe. The message can be
   # anything that will Marshal cleanly
   def write(message)
@@ -49,7 +81,7 @@ server = PipeServer.new
 server_pid = fork do
   server.callee!
 
-  while message = server.read
+  server.process_messages do |message|
     puts "SERVER READ message: #{message}"
 
     if message == "exit"
